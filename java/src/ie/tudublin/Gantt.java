100f4d7198caf091f47ed7e05aaac88a0532c430
FunszeEvil <danielfoley1122@hotmail.com>
2020-04-22 12:58:14 +0100

    added the task names to the y axis

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -58,23 +58,35 @@ public class Gantt extends PApplet
 	{
 		//create a border to work off
 		float border = width * 0.05f;
-		float textBox = width * 0.33f;
+		float textBox = width * 0.20f;
+		float textBoxHeight = height * 0.5f;
 		int numOfDays = 30;
 
 
 		pushMatrix();
 		stroke(0, 0 ,255);
 		textAlign(CENTER, CENTER);
+		// Draws the horizontal grid 
 		for(int i = 0; i < numOfDays; i++){
-			// Draws the horizontal grid 
 			float x = map(i, 0, 30, textBox, width - border);
 			line( x, border, x, height - border);
 			fill(0);
         	textSize(10);
-        	text(i + 1, x, 5);
+        	text(i + 1, x, border - 10);
 
 		}
 		popMatrix();
+
+		//print out the names of the tasks
+		//
+		for(int i = 0; i < tasks.size() ; i ++){
+			float y = map(i , 0, tasks.size(), textBoxHeight / 3, textBoxHeight * 1.5f );
+			textSize(15);
+			text(tasks.get(i).getTaskName(), border + 50, y);
+		}
+
+
+
 	}
 	
 	public void setup() 
