a9861ddd1302e5431d7bd1ec946f3b1b92a63d30
FunszeEvil <danielfoley1122@hotmail.com>
2020-04-22 10:33:18 +0100

    Created ArratList task with instance of tasks that has data loaded from the csv. the data is then printed to the terminal.

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,10 +1,16 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+import processing.data.Table;
+import processing.data.TableRow;
 import processing.core.PApplet;
 
 public class Gantt extends PApplet
 {	
-	
+	//Create arraylist for task
+	//
+	public ArrayList<Task> tasks = new ArrayList<Task>();
+
 	public void settings()
 	{
 		size(800, 600);
@@ -12,12 +18,24 @@ public class Gantt extends PApplet
 
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv","header");
+		//reads for each row in tasks and adds the row data to as a new 
+		// part of the ArrayList instnace of tasks
+		//
+		for(TableRow row:table.rows()){
+			Task t = new Task(row);
+			tasks.add(t);
+		}
 	}
 
 	public void printTasks()
 	{
-		
+		System.out.println("tasks to be completed \n");
+		// prints out each row of the ArrayList to terminal
+		//
+		for(Task t:tasks){
+			System.out.println(t);
+		}
 	}
 	
 	public void mousePressed()
@@ -34,6 +52,8 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
 	}
 	
 	public void draw()
